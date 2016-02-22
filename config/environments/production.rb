9747cc51da38710c0d3423a1a2e7b4dea73daa86
vagrant <vagrant@precise32.(none)>
2016-02-22 11:04:56 +0000

    (:

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,5 +76,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { :host => 'localhost.com' }
+  config.action_mailer.default_url_options = { :host => 'pacific-eyrie-26150.herokuapp.com' }
 end
